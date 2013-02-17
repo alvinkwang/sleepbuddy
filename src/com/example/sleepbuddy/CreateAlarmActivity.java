63a2512049c7f9d05671dc95c39d38287a757880
Alvin Kwang <whoisalvin@gmail.com>
2013-02-17 21:44:14 +0800

    added dialog box for GameType in alarm settings

--- src/com/example/sleepbuddy/CreateAlarmActivity.java
+++ src/com/example/sleepbuddy/CreateAlarmActivity.java
@@ -1,6 +1,8 @@
 package com.example.sleepbuddy;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
@@ -9,36 +11,71 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class CreateAlarmActivity extends ListActivity {
 
-	static final String[] ALARM_SETTINGS = new String[] {"Alarm Repeat", "Snooze", "Game Type", "SMS Buddy"};
-	static final boolean[] ALARM_SETTINGS_ICON = {true, true, true, true};
-	
+	static final String[] ALARM_SETTINGS = { "Alarm Repeat", "Snooze Duration", "Game Type", "SMS Buddy" };
+	static final boolean[] ALARM_SETTINGS_ICON = { true, true, true, true };
+
+	static final String[] GAME_TYPE = { "Math Sum", "Captcha", "Shaker" };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_create_alarm);
+		// setContentView(R.layout.activity_create_alarm);
 		// Show the Up button in the action bar.
 		setupActionBar();
 
-		setListAdapter(new ImageAdapter(this, R.layout.activity_create_alarm, R.id.text1, R.id.image1, ALARM_SETTINGS, ALARM_SETTINGS_ICON));
-		
-//		setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_create_alarm, ALARM_SETTINGS));
-//		
+		setListAdapter(new ImageAdapter(this, R.layout.activity_create_alarm, R.id.text1, R.id.image1, ALARM_SETTINGS,
+				ALARM_SETTINGS_ICON));
+
+		// setListAdapter(new ArrayAdapter<String>(this,
+		// R.layout.activity_create_alarm, ALARM_SETTINGS));
+		//
 		ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
-		
-//		listView.setOnItemClickListener(new OnItemClickListener() {
-//			public void onItemClick(AdapterView<?> parent, View view,
-//					int position, long id) {
-//			    // When clicked, show a toast with the TextView text
-//			    Toast.makeText(getApplicationContext(),
-//				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-//			}
-//		});
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// When clicked, show a toast with the TextView text
+				switch (position) {
+
+				case 0: // Alarm Repeat
+					Toast.makeText(getApplicationContext(), "0", Toast.LENGTH_SHORT).show();
+					break;
+				case 1: // Snooze Duration
+					Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+					break;
+				case 2: // Game Type
+					buildGameTypeDialog();
+					break;
+				case 3: // SMS Buddy
+					// stub
+					break;
+				default:
+					Toast.makeText(getApplicationContext(), "default", Toast.LENGTH_SHORT).show();
+					break;
+				}
+			}
+		});
+	}
+
+	private void buildGameTypeDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(R.string.dialog_title_game_type);
+		builder.setNegativeButton(R.string.dialog_button_save, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				Toast.makeText(getApplicationContext(), "Save", Toast.LENGTH_SHORT).show();
+			}
+		});
+		builder.setPositiveButton(R.string.dialog_button_cancel, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+			}
+		});
+		AlertDialog dialog = builder.create();
+		dialog.show();
 	}
 
 	/**
